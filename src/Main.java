e3dacd61636a0af3f39a019a50f3e71db0be603b
sandi <pal.andrea14@gmail.com>
2020-11-30 09:46:44 +0200

    add solution for day 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -10,7 +12,7 @@ public class Main {
     static int m;
     static HashSet<Integer> list = new HashSet<>();
 
-    public static void main (String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         try {
             File myObj = new File("input");
             Scanner myReader = new Scanner(myObj);
@@ -30,16 +32,72 @@ public class Main {
         }
         System.out.println(result);
         System.out.println(m);
+        System.out.println(checksum());
+        System.out.println(differByOne());
     }
 
-    public static int calibrate(int input){
+    public static int calibrate(int input) {
         s += input;
         if (!(list.add(s))) {
             m = s;
             b = true;
-        };
+        }
+        ;
         return s;
     }
 
-}
+    public static int checksum() throws FileNotFoundException {
+        File myObjID = new File("inputIDs");
+        Scanner myReader = new Scanner(myObjID);
+        int a = 0;
+        int d = 0;
+        while (myReader.hasNextLine()) {
+            HashMap<Character, Integer> characterCount = new HashMap<>();
+            String data = myReader.nextLine();
+            for (char c : data.toCharArray()) {
+                if (characterCount.containsKey(c)) {
+                    characterCount.put(c, (characterCount.get(c)) + 1);
+                } else {
+                    characterCount.put(c, 1);
+                }
+            }
+            if (characterCount.containsValue(2)) {
+                a += 1;
+            }
+            if (characterCount.containsValue(3)) {
+                d += 1;
+            }
 
+        }
+        return a * d;
+    }
+
+    public static String differByOne() throws FileNotFoundException {
+        File myObjID = new File("inputIDs");
+        Scanner myReader = new Scanner(myObjID);
+
+        while (myReader.hasNextLine()) {
+
+            String dataA = myReader.nextLine();
+            Scanner Reader = new Scanner(myObjID);
+
+            while (Reader.hasNextLine()) {
+
+                String dataB = Reader.nextLine();
+                int number = 0;
+                String result = "";
+
+                for (int i = 0; i < dataA.length(); i++) {
+                    if (dataA.charAt(i) != dataB.charAt(i)) {
+                        number += 1;
+                    } else {
+                        result += dataA.charAt(i);
+                    }
+                }
+                if (number == 1) {
+                    return result;
+                }
+            }
+        } return "not found";
+    }
+}
